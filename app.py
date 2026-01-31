dc2660711fb2f4e4e77a6d85119f3cef38a7835e
a-raza321 <114188409+a-raza321@users.noreply.github.com>
2026-01-31 12:15:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -466,7 +466,7 @@ def save_analysis_to_bigquery(ticker, report_data, risk_reward, llm_data):
     try:
         # 1. Authenticate and Initialize Client
        credentials = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT_JSON)
-        client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+       client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
         # 2. DELETE EXISTING TABLE
         client.delete_table(table_id, not_found_ok=True)
@@ -922,4 +922,5 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
